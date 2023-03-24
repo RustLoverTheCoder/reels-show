60771a280b36f899ba734442966973332fe77a7b
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-03-24 20:44:33 +0800

    feat: add dayjs

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,10 @@
+import dayjs from "dayjs";
+
 const Footer: React.FC = () => {
-  const today = new Date();
+  const today = dayjs().format("YYYY");
   return (
     <footer className="prose px-4">
-      <p>&copy; {today.getFullYear()} YOUR NAME HERE. All rights reserved.</p>
+      <p>&copy; {today} YOUR NAME HERE. All rights reserved.</p>
     </footer>
   );
 };
